1b97207c4c8d5130c276cd3ec0b3274cf72a46e7
Mftr96 <matteo.federici96@gmail.com>
2024-04-16 15:43:37 +0200

    bonus 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const numero="#24";
 
+const msg="la tua password è: ";
+
 let nome= prompt("inserisci il tuo nome");
 
 let cognome=prompt("inserisci il cognome");
@@ -7,3 +9,8 @@ let cognome=prompt("inserisci il cognome");
 let colore=prompt("inserisci il colore preferito");
 
 document.getElementById("bucata").innerHTML= nome+cognome+colore+numero;
+
+let secret=nome+cognome+colore+numero;
+
+document.getElementById("secret-psw").innerHTML= msg+secret;
+
